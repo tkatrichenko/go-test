14325c57bc096364f29911a75953e152cac26b6c
taras.katrichenko <taras.katrichenko@intellecteu.com>
2019-11-07 19:37:30 +0200

    first tries

---
+++ 02_var/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var test string = "testing"
+	var year = 37
+
+	var haha string = "3"
+	haha = "hoho"
+	// var a = 1
+	// var b = 5
+
+	fmt.Println(test, year)
+	fmt.Printf("var year is %T\n", year)
+	fmt.Println(haha)
+}
